f6393f37bd4fff6354b1525176b2e0d9af8a9012
Pedro Henrique Coan Zin <115580352+ZinPedro@users.noreply.github.com>
2025-08-22 11:00:31 -0300

    inicio ex2

---
+++ Lab03 (Listas lineares)/Exercicio2.c
@@ -0,0 +1,275 @@
+/*Vamos complicar um pouquinho, criando outras funções de
+Lista Linear (utilizar ex1 como base
+• Inserção de um Elemento da Lista (inserir no fim da lista)
+• Remoção de um Elemento da Lista (retirar do fim da lista)
+• Uma função que retorna a quantidade de números ímpares da
+lista
+• Uma função que receba duas listas e retorne uma terceira lista
+que é a união dessas duas listas (Não é permitido elementos
+repetidos na Lista União)*/
+
+//incabado
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+    int info;
+    struct no *prox;
+}no;
+
+typedef no Lista;
+
+Lista* Inicializa (){
+    return NULL;
+}
+
+Lista* InsereInicio (Lista *aux,int v){
+    Lista *novo;
+
+    novo = (Lista*) malloc(sizeof(Lista));
+    if(novo != NULL){
+        novo -> info = v;
+        novo -> prox = aux;
+        printf("Valor '%d' adcionado com exito!",v);
+        return novo;
+    }
+    printf("Memoria cheia.");
+    return aux;
+}
+
+Lista* InsereFim (Lista *aux,int v){
+    Lista *novo;
+    Lista *aux2;
+    aux2 = aux -> prox;
+
+    novo = (Lista*) malloc(sizeof(Lista));
+    if(novo != NULL){
+        novo -> info = v;
+        novo -> prox = NULL;
+
+        while(aux2 -> prox != NULL){
+            aux2 = aux2 -> prox;
+        }
+
+        aux2 -> prox = novo;
+
+        printf("Valor '%d' adcionado com exito!",v);
+        return aux;
+    }
+    printf("Memoria cheia.");
+    return aux;
+}
+
+Lista* ExcluiInicio (Lista *aux){
+    if (aux != NULL){
+        Lista *aux2;
+        aux2 = aux;
+        aux = aux -> prox;
+        free(aux2);
+
+        printf("Elemento do inicio da lista excluido!");
+        return aux;
+    }
+    printf("A lista ja esta vazia!");
+    return aux;
+}
+
+Lista* ExcluiFim (Lista *aux){
+    if (aux != NULL){
+        Lista *aux2;
+        aux2 = aux;
+        while(aux2 -> prox != NULL){
+            aux2 = aux2 -> prox;
+        }
+        free(aux2);
+
+        printf("Elemento do fim da lista excluido!");
+        return aux;
+    }
+    printf("A lista ja esta vazia!");
+    return aux;
+}
+
+void Imprime (Lista *aux){
+    if(aux == NULL){
+        printf("A lista esta vazia!");
+    }else {
+
+        printf("Lista: ");
+        while(aux != NULL){
+            printf("%d ",aux -> info);
+            aux = aux -> prox;
+        }
+    }
+    return;
+}
+
+int Busca (Lista *aux,int valor){
+    while (aux != NULL){
+        if (aux -> info == valor){
+            return 1;
+        }
+        aux = aux -> prox;
+    }
+
+    return 0;
+}
+
+int ListaVazia (Lista *aux){
+    if (aux == NULL){
+        return 1;
+    }else {
+        return 0;
+    }
+}
+
+int nElemenntosLista(Lista *aux){
+    int cnt=0;
+    while(aux != NULL){
+        cnt++;
+        aux = aux -> prox;
+    }
+    return cnt;
+}
+
+int somaLista(Lista *aux){
+    int soma=0;
+    while(aux != NULL){
+        soma += aux->info;
+        aux = aux -> prox;
+    }
+    return soma;
+}
+
+
+
+int main (){
+    int op,verifica,v;
+    int sair = 0;
+
+
+    Lista *L;
+    L = Inicializa();
+
+    while (sair != 1){
+
+        printf("\t\t\t---------------------------------------------------------");
+        printf("\n\t\t\t | MENU |");
+        printf("\n\t\t\t---------------------------------------------------------");
+        printf("\n\t\t\t | Opcoes: ");
+        printf("\n\t\t\t\t  1. Inserir valor (inteiro) no inicio da lista: ");
+        printf("\n\t\t\t\t  2. Inserir valor (inteiro) no final lista: ");
+        printf("\n\t\t\t\t  3. Remover valor do inicio da lista: ");
+        printf("\n\t\t\t\t  4. Remover valor do final da lista: ");
+        printf("\n\t\t\t\t  5. Imprimir lista: ");
+        printf("\n\t\t\t\t  6. Buscar valor na lista: ");
+        printf("\n\t\t\t\t  7. Verificar se a lista esta vazia: ");
+        printf("\n\t\t\t\t  8. Verificar o numero de elementos da lista: ");
+        printf("\n\t\t\t\t  9. Soma dos elementos da lista: ");
+        printf("\n\t\t\t\t 10. Quantidade de impares da lista: ");
+        printf("\n\t\t\t\t 11. Em Desenvolvimento... ");
+        printf("\n\t\t\t\t 12. Sair. ");
+        printf("\n\t\t\t---------------------------------------------------------");
+        printf("\n\t\t\tDigite o numero referente a opcao desejada: ");
+        scanf("%d",&op);
+
+        if(op < 1 || op > 12){
+            system("cls");
+            printf("\t\t\t---------------------------------------------------------");
+            printf("\n\t\t\tOpcao invalida. Tente novamente!\n");
+            continue;
+        }
+
+        switch (op){
+            case 1:
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\tDigite o valor que voce quer inserir no inicio da lista: ");
+                scanf("%d",&v);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\t");
+                L = InsereInicio(L,v);
+                printf("\n");
+            break;
+            case 2:
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\tDigite o valor que voce quer inserir no final da lista: ");
+                scanf("%d",&v);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\t");
+                L = InsereFim(L,v);
+                printf("\n");
+            break;
+            case 3:
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\t");
+                L = ExcluiInicio(L);
+                printf("\n");
+            break;
+            case 4:
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\t");
+                L = ExcluiFim(L);
+                printf("\n");
+            break;
+            case 5:
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\t");
+                Imprime(L);
+                printf("\n");
+            break;
+            case 6:
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\tQual o valor que voce quer buscar: ");
+
+                scanf("%d",&v);
+                verifica = Busca(L,v);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                if (verifica == 1){
+                    printf("\n\t\t\tO valor %d existe na lista!\n",v);
+                }else if(verifica == 0){
+                    printf("\n\t\t\tO valor %d nao existe na lista!\n",v);
+                }else{printf("\n\t\t\tErro!");}
+            break;
+            case 7:
+                verifica = ListaVazia(L);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                if (verifica == 1){
+                    printf("\n\t\t\tA lista esta vazia!\n");
+                }else if(verifica == 0){
+                    printf("\n\t\t\tA lista nao esta vazia\n");
+                }else{printf("\n\t\t\tErro!");}
+            break;
+            case 8:
+                verifica = nElemenntosLista(L);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\tO tamanho da lista eh de %d elementos\n",verifica);
+            break;
+            case 9:
+                verifica = somaLista(L);
+                system("cls");
+                printf("\t\t\t---------------------------------------------------------");
+                printf("\n\t\t\tA soma de todos os elementos da lista eh igual a: %d\n",verifica);
+            break;
+            case 10:
+
+            break;
+            case 11:
+
+            break;
+            case 12:
+                sair = 1;
+            break;
+        }
+    }
+
+    return 0;
+}
