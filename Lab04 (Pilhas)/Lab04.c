e11d49e84695316a589f6e1a30f070a9635c5fdc
Pedro Henrique Coan Zin <115580352+ZinPedro@users.noreply.github.com>
2025-09-01 09:31:38 -0300

    lab04 continua

--- Lab04 (Pilhas)/Lab04.c
+++ Lab04 (Pilhas)/Lab04.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Pilha.h"
+/*
+1. Conte os Elementos da Pilha
 
+2. Inverta a Pilha
+
+3. Remova um determinado elemento da pilha, se existir
+
+4. Dado duas pilhas, passadas por parâmetro, retorne qual tem mais elementos. Os valores de retorno serão:
+• 0 se as duas pilhas tiverem a mesma quantidade de elementos
+• 1 se a pilha 1 tiver mais elementos
+• 2 se a pilha 2 tiver mais elementos
+O cabeçalho da função deverá ser: int comapara(Lista *p1, Lista *p2)
+
+5. Leia um número inteiro e guarde os seus dígitos numa pilha. O cabeçalho da função deverá ser: Pilha *Digitos(int num)
+
+6. Dado um número inteiro, cujo os seus dígitos estão armazenados numa pilha, imprima o número inteiro
+*/
 //Inacabado
+
+int ContaPilha (Pilha *p){
+    int cnt=0;
+    No* aux = p -> Topo;
+    while (aux != NULL){
+        cnt++;
+        aux = aux->prox;
+    }
+    printf("Numero de elementos da Pilha: %d",cnt);
+    return cnt;
+}
+
+void InvertePilha (Pilha *p){
+    int aux[ContaPilha(p)];
+    int i=0;
+
+    while(!vaziaPilha(p)){
+        aux[i] = Pop(p);
+        i++;
+    }
+    for(int j = 0; j < i; j++){
+        Push(p,aux[j]);
+    }
+
+    return;
+}
 
 int main (){
         int v=0;
@@ -12,8 +54,8 @@ int main (){
         printf("\n\t\t\t | MENU |");
         printf("\n\t\t\t---------------------------------------------------------");
         printf("\n\t\t\t | Opcoes: ");
-        printf("\n\t\t\t\t 1. Inserir valor (inteiro) na lista: ");
-        printf("\n\t\t\t\t 2. Remover ultimo valor inserido da lista: ");
+        printf("\n\t\t\t\t 1. Numero de elementos da Pilha: ");
+        printf("\n\t\t\t\t 2. Inverter a Pilha: ");
         printf("\n\t\t\t\t 3. Imprimir lista: ");
         printf("\n\t\t\t\t 4. Buscar valor na lista: ");
         printf("\n\t\t\t\t 5. Verificar se a lista esta vazia: ");
@@ -26,18 +68,34 @@ int main (){
 
         P = CriaPilha();
         Push(P,v);
-        Push(P,6);
+        Push(P,4);
         Push(P,5);
+        Push(P,13);
+        Push(P,22);
+        Push(P,7);
+        Push(P,5);
+        Push(P,9);
+        Push(P,14);
         printf("\n\t\t\t");
         Imprime(P);
+        InvertePilha(P);
+        printf("\n\t\t\t");
+        Imprime(P);
+        printf("\n\t\t\t---------------------------------------------------------\n");
+        printf("\n\t\t\t");
+        ContaPilha(P);
 
         printf("\n\t\t\t---------------------------------------------------------\n");
         v = Pop(P);
         printf("\n\t\t\t%d",v);
         printf("\n\t\t\t---------------------------------------------------------\n");
         printf("\n\t\t\t");
+        ContaPilha(P);
+        printf("\n\t\t\t");
         Imprime(P);
         printf("\n\t\t\t---------------------------------------------------------\n");
+        printf("\n\t\t\t");
         liberaPilha(P);
+
 
 }
